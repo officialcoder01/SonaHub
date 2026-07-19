6bb404e5471c8c15b16db6b3534be629a945d2d8
Zion Musa <musazion986@gmail.com>
2026-07-19 17:07:41 +0100

    top rated vendors api endpoint

---
+++ frontend/src/services/recommendationService.js
@@ -0,0 +1,17 @@
+const API_URL = "http://localhost:3000/api/recommendations";
+
+const handleResponse = async (response) => {
+    const result = await response.json();
+
+    if (!result.ok) {
+        throw new Error(result.message || "Request failed");
+    };
+
+    return result;
+}
+
+const topRatedVendors = async () => {
+    const response = await fetch(`${API_URL}/top-rated-vendors`);
+
+    return handleResponse(response);
+}
